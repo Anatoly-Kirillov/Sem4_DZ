2a269147bfc20628f0c0d20d751ed4c65bc93ab3
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-09-02 01:11:37 +0300

    Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+void SumNumbers(int num)
+{
+    int num1 = num % 10;
+    int num2 = num / 10 % 10;
+    int num3 = num / 100 % 10;
+    int num4 = num / 1000;
+    if (num < 10000 && num > 0)
+    {
+        int number = num4 + num3 + num2 + num1;
+        Console.WriteLine($"Сумма цифр числа {num} равна: {number}");
+    }
+}
+Console.WriteLine("Введите число X: ");
+int number = int.Parse(Console.ReadLine());
+SumNumbers(number);
